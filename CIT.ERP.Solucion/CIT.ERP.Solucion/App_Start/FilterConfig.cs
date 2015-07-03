16c411c5465efe3cfedc5a673dfb3365fcfdd598
Victor Canal <victor.canal@criteriait.com>
2015-07-03 15:14:51 -0500

    Primera Versión

---
+++ CIT.ERP.Solucion/CIT.ERP.Solucion/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CIT.ERP.Solucion
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CIT.ERP.Solucion/CIT.ERP.Solucion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CIT.ERP.Solucion.MvcApplication" Language="C#" %>
